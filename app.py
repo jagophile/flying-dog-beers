66e9761e4bd54253d772b4d176191d405e3ed9ed
Jagophile <adam.r.foster@gmail.com>
2020-01-05 12:01:27 -0500

    t1

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import numpy
+
+import pickle
 
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
